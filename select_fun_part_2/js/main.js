55da2957c248064d52035e7eb1696ea47dbe81a6
Your Name <you@example.com>
2016-04-05 08:39:51 -0700

    Select fun 2 (not done)

--- select_fun_part_2/js/main.js
+++ select_fun_part_2/js/main.js
@@ -1,21 +1,28 @@
 // 1.	Slide the header element up when it is clicked on.
-
+$('header').on('click', function(){
+    $('header').slideUp();
+}); 
 // 2.	Remove the purplebox class from everything.
-
+$('.purplebox').removeClass('purplebox')
 // 3.	Hide all of the div’s in #row4
-
+$('#row4').hide('divs')
 // 4.	Add a paragraph that says “I have been added” to all boxes with class “add-para” (Hint: you'll need to look up the append() method)
-
+$('.add-para').click(function(){
+  $("p").append("I have been added");
+})
 // 5.	Add a class in your css and add it to all items with the class .box
-
+$('.box').addClass('dope');
 // 6.	Show the footer when the anchor in #box2 is clicked
-
+$('#box2 a').click(function(){
+    $('footer').show(300);
+})
 // 7.	Change the styling of the span inside of #box3 by adding a class (and style that class in CSS)
-
+$('#box3 span').addClass('awesome');
+$('.awesome').css('color', '#B72B42');
 // 8.	Add a unique class to your CSS and add it to the articles in #row1
-
+$('#row1').addClass('unique');
 // 9.	Change the background color of all the articles
-
+$('article').css('backgroundColor','#6Ab56F');
 // 10.	Change the font color of all the articles in row 2
 
 // 11.	Select any spans that are siblings of an h4 (using the .siblings() method) and hide them.
