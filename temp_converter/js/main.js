b36d9663deb6f43b36c69e68067f6369f51fb196
Your Name <you@example.com>
2016-04-08 08:27:26 -0700

    adding the classes

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -7,8 +7,39 @@ $('#fahrenheit_to_celsius').on('click', function (){
   var fahrenheit = $('#temperature').val();
   //Convert that value to a number
   fahrenheit = parseFloat(fahrenheit);
-  //Convert to celcius and store in variable celcius
-  var celcius = ((fahrenheit - 32) / 1.8); 
+  //Convert to celsius and store in variable celsius
+  var celsius = ((fahrenheit - 32) / 1.8); 
   //Display in #result
-  $('#result').html(fahrenheit + " Fahrenheit is " + celcius + " Celsius.");
+  $('#result').html(fahrenheit + " Fahrenheit is " + celsius + " Celsius.");
+
+  if (fahrenheit < 32) {
+    $('body').addClass('chilly');
+  } else {
+    if (fahrenheit > 32) {
+      $('body').addClass('balmy');
+  }}
 });
+
+
+//When the user clicks the #celsius_to_fahrenheit button 
+$('#celsius_to_fahrenheit').on('click', function (){
+   //Get the value from #temperature and store in variable celsius
+   var celsius = $('#temperature').val();
+   //Convert that value to a number
+   celsius = parseFloat(celsius);
+   //Convert to fahrenheit and store in variable fahrenheit
+   var fahrenheit = (1.8 * celsius + 32);
+   //Display in #result
+   $('#result').html(celsius + " Celsius is " + fahrenheit + " Fahrenheit.");
+
+
+
+// function hotOrNot (temp) {
+//   if (temp > 75) {
+//     $('.hot').fadeIn(350);
+//   } else {
+//     $('.not-hot').fadeIn(350);
+//   }
+// }
+
+// hotOrNot(30);
